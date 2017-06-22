713ee2918b0af3996802a02bba3f7aa3264c8172
Javed Kansi <javed.kansi@aurea.com>
2017-06-22 08:48:50 +0500

    Added status and skip motor on to scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from dateutil import parser
 import logging
@@ -10,6 +10,7 @@ import sys,os
 sys.path.append(os.getcwd())
 
 from utils.jk_util import *
+from utils.http_server import *
 
 # read config file
 map = read_config("config.yaml", "SCHEDULER")
@@ -17,8 +18,40 @@ map = read_config("config.yaml", "SCHEDULER")
 logging.basicConfig(filename='log/application.log', filemode='a', format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.WARNING)
 
+
 lightsOffJob = None
 lightsOnJob = None
+countToSkipMotorOn = 0
+sunriseTime = None
+sunsetTime = None
+lightsOffTime = None
+lightsOnTime = None
+lightsOnTime = None
+
+
+class SchedulerServer(JKHttpHandler):
+    def do_GET(self):
+        params = parse_query_params(self)
+
+        # for testing purposes only
+        if params.get('test_motor_on'):
+            turn_water_motor_on()
+
+        global countToSkipMotorOn
+
+        response = "Sunrise: " + sunriseTime.strftime("%H:%M:%S")
+        response += "\nSunset: " + sunsetTime.strftime("%H:%M:%S")
+        response += "\nLights Off: " + lightsOffTime.strftime("%H:%M:%S")
+        response += "\nLights On: " + lightsOnTime.strftime("%H:%M:%S")
+        response += "\nTimes to skip motor on: "
+
+        if params.get('skip_motor_on'):
+            countToSkipMotorOn = int(params.get('skip_motor_on')[0])
+
+        response += str(countToSkipMotorOn)
+
+        self._set_headers()
+        self.wfile.write(response)
 
 
 def turn_lights_on():
@@ -32,8 +65,15 @@ def turn_lights_off():
 
 
 def turn_water_motor_on():
-    logging.warning("Turning the water motor on...")
-    water_motor_on()
+    global countToSkipMotorOn
+
+    if countToSkipMotorOn > 0:
+        countToSkipMotorOn -= 1
+        logging.warning("Not going to turn on water motor. Current count to skip motor on is " + str(countToSkipMotorOn))
+
+    else:
+        logging.warning("Turning the water motor on...")
+        water_motor_on()
 
 
 def turn_water_motor_off():
@@ -56,13 +96,15 @@ def sunset_sunrise_job_scheduler():
     serverTimezone = pytz.timezone("UTC")
     pakistanTz = pytz.timezone("Asia/Karachi")
 
-    sunrise = serverTimezone.localize(srtime).astimezone(pakistanTz)
-    sunset = serverTimezone.localize(sstime).astimezone(pakistanTz)
+    global sunriseTime, sunsetTime, lightsOffTime, lightsOnTime
 
-    send_lcd_screen_request("SR: " + sunrise.strftime("%H:%M:%S") + "\nSS: " + sunset.strftime("%H:%M:%S"))
+    sunriseTime = serverTimezone.localize(srtime).astimezone(pakistanTz)
+    sunsetTime = serverTimezone.localize(sstime).astimezone(pakistanTz)
 
-    lightsOffTime = sunrise - dt.timedelta(minutes=map.get("TIME_BEFORE_SUNRISE"))
-    lightsOnTime = sunset - dt.timedelta(minutes=map.get("TIME_BEFORE_SUNSET"))
+    send_lcd_screen_request("SR: " + sunriseTime.strftime("%H:%M:%S") + "\nSS: " + sunsetTime.strftime("%H:%M:%S"))
+
+    lightsOffTime = sunriseTime - dt.timedelta(minutes=map.get("TIME_BEFORE_SUNRISE"))
+    lightsOnTime = sunsetTime - dt.timedelta(minutes=map.get("TIME_BEFORE_SUNSET"))
 
     global lightsOffJob
     global lightsOnJob
@@ -81,7 +123,7 @@ def sunset_sunrise_job_scheduler():
     logging.warning("Lights will be turned on at: " + str(lightsOnTime.hour) + ":" + str(lightsOnTime.minute));
 
 
-scheduler = BlockingScheduler()
+scheduler = BackgroundScheduler()
 
 # job to schedule lights on/off job on a daily basis based on sunrise and sunset
 lightsSchedulingJob = scheduler.add_job(sunset_sunrise_job_scheduler, 'cron', hour=12, id='Lights Scheduler')
@@ -96,3 +138,6 @@ waterMotorOffJob = scheduler.add_job(turn_water_motor_off, 'cron', hour='1,5,9,1
 # run the job once to schedule after a restart
 sunset_sunrise_job_scheduler()
 scheduler.start()
+
+if __name__ == "__main__":
+    run_http_server(handler_class=SchedulerServer, port=map.get("SERVER_PORT"))
